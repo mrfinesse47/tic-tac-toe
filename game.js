b05cbb487d2234bf9016eed7136441de3eb86d8b
Kevin Mason <krmason2020@gmail.com>
2022-02-28 18:29:35 -0800

    updated to have multiple svg for different fills to be use as img in the html

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 //will control the views and the game
 
-alert("testing script");
+//alert("testing script");
 
-const x = document.querySelector("#x");
-x.style.background = "pink";
-const o = document.querySelector("#o");
-o.classList.remove("menu-selected-character");
+// const x = document.querySelector("#x");
+// x.style.background = "pink";
+// const o = document.querySelector("#o");
+// o.classList.remove("menu-selected-character");
