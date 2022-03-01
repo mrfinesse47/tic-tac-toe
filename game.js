05ca560fa3554968d8779313b332c367d31e1007
Kevin Mason <krmason2020@gmail.com>
2022-02-28 19:59:38 -0800

    able to toggle selection now

--- game.js
+++ game.js
@@ -7,8 +7,39 @@
 // const o = document.querySelector("#o");
 // o.classList.remove("menu-selected-character");
 
-const game = () => {};
+const game = { p1Mark: "o" }; //accessable to all functions that come after.
 
-const mainMenuController = () => {};
+const toggleSelection = (el) => {
+  const letter = el.id;
+  const o = document.querySelector("#o");
+  const x = document.querySelector("#x");
+  const oImg = document.querySelector("#o img");
+  const xImg = document.querySelector("#x img");
 
-game();
+  //console.log(oImg.src);
+
+  if (el.id === "x") {
+    o.classList.remove("menu-selected-character");
+    x.classList.add("menu-selected-character");
+
+    game.p1Mark = "x";
+
+    console.log(xImg.src);
+
+    xImg.src = xImg.src.replace("silver", "dark-navy");
+    oImg.src = oImg.src.replace("dark-navy", "silver");
+
+    console.log(xImg.src);
+  }
+  if (el.id === "o") {
+    x.classList.remove("menu-selected-character");
+    o.classList.add("menu-selected-character");
+
+    game.p1Mark = "o";
+
+    xImg.src = xImg.src.replace("dark-navy", "silver");
+    oImg.src = oImg.src.replace("silver", "dark-navy");
+
+    // console.log(xImg.src);
+  }
+};
